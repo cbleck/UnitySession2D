4fcb6b84fc63a8f3bc93da715a655f5004b9b6b7
cbleck <carlos@capptu.com>
2016-07-22 20:45:35 -0500

    fb share

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour 
+{
+	public GameObject facebookComposerPanel;
+	public GameObject shareButton;
+	public Image screenshotPreview;
+
+	void Start () 
+	{
+		facebookComposerPanel.SetActive (false);
+		shareButton.SetActive (true);
+	}
+	
+	public void ShareToFacebook()
+	{		
+		shareButton.SetActive (false);
+        Utilities.instance.captureScreenShotCallback += ScreenShotCallback;
+        Utilities.instance.TakeScreenShot();
+        Time.timeScale = 0f;
+	}
+
+	public void CancelShare()
+	{
+		facebookComposerPanel.SetActive (false);
+		shareButton.SetActive (true);
+        Time.timeScale = 1f;
+    }
+
+	public void ConfirmShareToFacebook()
+	{
+		facebookComposerPanel.SetActive (false);
+		shareButton.SetActive (true);
+        Time.timeScale = 1f;
+        FacebookManager.instance.ShareMessage("Compartir"/*messageToShare.text*/, Utilities.instance.screenShot);
+    }
+
+	public void ScreenShotCallback()
+	{
+        screenshotPreview.sprite = Sprite.Create(Utilities.instance.screenShot,
+                                                 new Rect(0,
+                                                          0,
+                                                          Utilities.instance.screenShot.width,
+                                                          Utilities.instance.screenShot.height),
+                                                 new Vector2(0.5f,0.5f));
+		facebookComposerPanel.SetActive (true);
+        Utilities.instance.captureScreenShotCallback -= ScreenShotCallback;
+    }
+
+    public void LoginToFacebook() {
+        FacebookManager.instance.LogIn();
+    }
+}
